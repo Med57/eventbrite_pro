fe8edd15fec69a36740efd015d2b4378c317c56a
Med57 <medhy_daas_67@msn.com>
2020-05-13 20:30:28 +0200

    add event controller

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.5.1'
 gem 'faker'
 
 gem 'dotenv-rails'
-
+gem 'devise'
 gem 'therubyracer'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,7 @@
 class Event < ApplicationRecord
-  has_many :users, through: :attendance
   has_many :attendances
+  has_many :users, through: :attendance
   belongs_to :user
 
-  validates :start_date, presence: true
-
-  validates :duration, presence: true, numericality: {odd: 5 }
-
-  validates :title, presence: true, length: {in: 5..140}
-
-  validates :description, presence: true, length: {in: 20..1000}
-
-  validates :price, presence:true, length: {in: 1..1000}, numericality: {greater_than_or_equal_to: 1 }
-
-  validates :location, presence:true
 
 end
